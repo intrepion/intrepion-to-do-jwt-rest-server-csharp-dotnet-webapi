94be38c6efbf817be4f70544b9d0dc94fd6ec651
Oliver Forral <intrepion@gmail.com>
2023-01-20 20:23:07 -0700

    red - testing the health check controller

---
+++ ToDoApp.Tests/WebApi/HealthCheck/TestHealthCheckController.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.WebApi.HealthCheck;
+
+namespace ToDoApp.Tests.WebApi.HealthCheck;
+
+public class TestHealthCheckController
+{
+    [Fact]
+    public void Get_Returns200()
+    {
+        // Arrange
+        var controller = new HealthCheckController();
+
+        // Act
+        var actualResult = controller.Get();
+
+        // Assert
+        actualResult.Should().BeOfType<OkObjectResult>();
+        var okObjectResult = (OkObjectResult)actualResult;
+        okObjectResult.StatusCode.Should().Be(200);
+    }
+}
