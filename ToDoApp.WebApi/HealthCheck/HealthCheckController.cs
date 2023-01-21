2fd79a368a621536d4c7c68d295b1d69bb9e42cd
Oliver Forral <intrepion@gmail.com>
2023-01-20 20:23:47 -0700

    green - testing the health check endpoint

--- ToDoApp.WebApi/HealthCheck/HealthCheckController.cs
+++ ToDoApp.WebApi/HealthCheck/HealthCheckController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ToDoApp.WebApi.HealthCheck;
 
+[ApiController]
+[Route("[controller]")]
 public class HealthCheckController : ControllerBase
 {
     public IActionResult Get()
